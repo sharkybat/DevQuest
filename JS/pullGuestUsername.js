43db3950988e3f4117e4f360fe59c024c0a1abe0
Sharky <eriktherdev@gmail.com>
2026-03-12 13:14:47 -0400

    Simplify submitInfo function by removing comments

--- JS/pullGuestUsername.js
+++ JS/pullGuestUsername.js
@@ -1,12 +1,5 @@
 function submitInfo() {
-	//works
 	let guestUsername = document.getElementById("guestuname").value;
 	localStorage.setItem("GuestUser", guestUsername);
-	
-	//window.alert(guestUsername);
-	
-	//works
-	//let retrieve = localStorage.getItem("GuestUser")
-	//window.alert(retrieve);
-
 }
+
