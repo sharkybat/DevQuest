1925a4dc0e4cd0b9e03398de085854fd460aa277
Sharky <eriktherdev@gmail.com>
2026-03-12 13:07:09 -0400

    update notes
    Commented out alert and retrieval of guest username.

--- JS/pullGuestUsername.js
+++ JS/pullGuestUsername.js
@@ -3,9 +3,10 @@ function submitInfo() {
 	let guestUsername = document.getElementById("guestuname").value;
 	localStorage.setItem("GuestUser", guestUsername);
 	
-	window.alert(guestUsername);
+	//window.alert(guestUsername);
 	
-	//doesnt work for some reason
-	let retrieve = localStorage.getItem("GuestUser")
-	window.alert(retrieve);
+	//works
+	//let retrieve = localStorage.getItem("GuestUser")
+	//window.alert(retrieve);
+
 }
